48e132d1f05d5876d385175de993cf2eee039129
juanestd <juanestd1013@hotmail.com>
2023-04-02 15:51:21 -0500

    Se inicia el proyecto, se conecta la base de datos y se comienza la interfaz

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  FIREBASE_CONFIG:{
+    apiKey: "AIzaSyCRlbEkFRZxO5X3D03ytvKy0PgNGaiNf-I",
+  authDomain: "appionictdea-16692.firebaseapp.com",
+  projectId: "appionictdea-16692",
+  storageBucket: "appionictdea-16692.appspot.com",
+  messagingSenderId: "405375683222",
+  appId: "1:405375683222:web:d9223600cd5f9397d650b7"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  FIREBASE_CONFIG:{
+    apiKey: "AIzaSyCRlbEkFRZxO5X3D03ytvKy0PgNGaiNf-I",
+    authDomain: "appionictdea-16692.firebaseapp.com",
+    projectId: "appionictdea-16692",
+    storageBucket: "appionictdea-16692.appspot.com",
+    messagingSenderId: "405375683222",
+    appId: "1:405375683222:web:d9223600cd5f9397d650b7"
+  }
 };
 
 /*
